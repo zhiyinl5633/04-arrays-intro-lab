6eb7ba23f459138cabf0b60f306eebcfbd8666cd
Kelly Lai <zhiyinl5633@hstat.org>
2017-10-26 16:04:41 +0000

    fix errors

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+    []
+end
+
+def array_with_two_elements
+    [1,2]
+end
+
+def first_element(my_array)
+    my_array[0]
+end
+
+def third_element(my_array)
+    my_array[2]
+end
+
+def last_element(my_array)
+    my_array[-1]
+end
+
+def first_element_with_array_methods(my_array)
+    my_array.first
+end
+
+def last_element_with_array_methods(my_array)
+    my_array.last
+end
+
+def length_of_array(my_array)
+    my_array.length
+end
